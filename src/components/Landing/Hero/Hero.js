12ab30843be8699e7ee4d158785d1f7c28af3d68
patrickngure45 <ngurengure10@gmail.com>
2021-11-05 02:46:16 +0300

    changed hero info  list-style

--- src/components/Landing/Hero/Hero.js
+++ src/components/Landing/Hero/Hero.js
@@ -12,16 +12,16 @@ const Hero = () => {
         </SectionTitle>
         <SectionText>
           <ul>
-            <li style={{ listStyle: 'inside' }}>
-              Two years experience working with React, JavaScript, HTML/CSS to
+            <li>
+              - Two years experience working with React, JavaScript, HTML/CSS to
               deliver exceptional user experiences.
             </li>
-            <li style={{ listStyle: 'inside' }}>
-              Adept at contributing to a highly collaborative environment,
+            <li>
+              - Adept at contributing to a highly collaborative environment,
               finding solutions, and delivering customer satisfaction.
             </li>
-            <li style={{ listStyle: 'inside' }}>
-              Enjoys challenges, thrives in teams, hard worker, excellent
+            <li>
+              - Enjoys challenges, thrives in teams, hard worker, excellent
               communicator, and strong writing skills.
             </li>
           </ul>
